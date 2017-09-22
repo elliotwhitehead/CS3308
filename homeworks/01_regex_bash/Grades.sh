66bec41a609da1e2f6b4157c4c6b4c577a9f8a17
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-09-22 17:52:43 -0600

    Complete Grades.sh script functionality

--- homeworks/01_regex_bash/Grades.sh
+++ homeworks/01_regex_bash/Grades.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 while read ID FIRST LAST SCORE1 SCORE2 SCORE3; do
-	echo (
-done < $1
+	avg=`echo "($SCORE1 + $SCORE2 + $SCORE3) / 3" | bc`
+	echo $avg [$ID] $LAST $FIRST
+done < $1 | sort -k 3 -k 4
