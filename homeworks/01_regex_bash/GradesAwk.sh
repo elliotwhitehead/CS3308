73277ef09c2e0fad5851e084b974c4b11b6d65f1
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-09-22 18:30:50 -0600

    Add grade calculation, sorting and printing with awk

--- homeworks/01_regex_bash/GradesAwk.sh
+++ homeworks/01_regex_bash/GradesAwk.sh
@@ -1,2 +1,13 @@
 #! /bin/bash
-awk '{ sum=0; for (col=4; col<=6; col++) sum+=$col; print int(sum/3); }' $1
+awk '{
+	sum=0;
+	for(col=4; col<=6; col++)
+		sum+=$col;
+		printf int(sum/3);
+		printf " [";
+		printf $1;
+		printf "] ";
+		printf $3;
+		printf ", ";
+		print $2;
+}' $1 | sort -k 3 -k 2
