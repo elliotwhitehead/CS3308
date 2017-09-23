9f535565f693fdd18011ea9b538e74913b14f14d
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-09-22 18:21:05 -0600

    Add comma to name output

--- homeworks/01_regex_bash/Grades.sh
+++ homeworks/01_regex_bash/Grades.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 while read ID FIRST LAST SCORE1 SCORE2 SCORE3; do
 	avg=`echo "($SCORE1 + $SCORE2 + $SCORE3) / 3" | bc`
-	echo $avg [$ID] $LAST $FIRST
+	echo $avg [$ID] "$LAST," $FIRST
 done < $1 | sort -k 3 -k 4
