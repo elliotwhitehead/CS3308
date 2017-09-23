eedec129e255618b1e52b7db906bf3af74ca8a82
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-09-23 00:05:56 -0600

    Add solutions to regexcercises

--- homeworks/01_regex_bash/RegexAnswers.sh
+++ homeworks/01_regex_bash/RegexAnswers.sh
@@ -7,3 +7,12 @@ elif [[ $# -gt 1 ]]; then
 	echo 'Usage: RegexAnswers.sh filename'
 	exit 0
 fi
+
+grep -c -E '[0-9]$' $1
+grep -c -E -v -i '^[aeiou]' $1
+grep -c -E -i '^[abcdefghijklmnopqrstuvwxyz]{12}$' $1
+grep -c -E '^[0-9]{3}[-][0-9]{3}[-][0-9]{4}$' $1
+grep -c -E '^303[-][0-9]{3}[-][0-9]{4}$' $1
+grep -c -E -i '^[aeiou].*[0-9]$' $1
+grep -c -E -i 'geocities.com$' $1
+grep -E '@' $1 | grep -E -v "\b[a-zA-Z0-9\.\_\-]+@[a-zA-Z0-9\.\_\-]+\.[a-zA-Z]{2,4}\b" | wc -l
