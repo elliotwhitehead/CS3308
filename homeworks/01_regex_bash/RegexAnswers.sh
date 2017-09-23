09d6d9412f6880874deb2725e79dc14866ecd1da
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-09-22 23:23:20 -0600

    Add command line usage warnings

--- homeworks/01_regex_bash/RegexAnswers.sh
+++ homeworks/01_regex_bash/RegexAnswers.sh
@@ -1,2 +1,9 @@
 #! /bin/bash
 # Author: Elliot Whitehead
+if [[ $# -eq 0 ]] ; then
+	echo 'Usage: RegexAnswers.sh filename'
+	exit 0
+elif [[ $# -gt 1 ]]; then
+	echo 'Usage: RegexAnswers.sh filename'
+	exit 0
+fi
